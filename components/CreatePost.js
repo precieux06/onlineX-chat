052a0406e7b5a7c352cf03de26b6de47f6af0971
precieux06 <jmakumbusela@gmail.com>
2025-09-24 03:15:39 +0100

    Create CreatePost.js

---
+++ components/CreatePost.js
@@ -0,0 +1,9 @@
+// components/CreatePost.js
+export default function CreatePost({ onPostCreated }) {
+  return (
+    <div>
+      <h3>Créer un post</h3>
+      {/* Votre formulaire de création de post */}
+    </div>
+  )
+}
